de3e2f1bfaa26cceb97339ada035ead16b55e55e
N0Fee1 <98322667+N0Fee1@users.noreply.github.com>
2022-02-17 23:07:09 +0700

    Update Roblox.Script.lua

--- Roblox.Script.lua
+++ Roblox.Script.lua
@@ -1,69 +1,89 @@
 local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Patskorn/GUI/main/Copy-SynapOver.lua"))()
-local GUI = library:new("","[ RightControl ]")
-local Main = GUI:Tap("Main")
-
-Main:Button("Goku",function()
-    local Fire = Instance.new("Fire")
-    Fire.Parent = game.Players.LocalPlayer.Character.HumanoidRootPart
-    Fire.Heat = 50
-    Fire.Size = 50
+local GUI = library:new("SynapOver","[ RightControl ]")
+local Fun = GUI:Tap("Funny")
+local Server = GUI:Tap("Server")
+Fun:TextBox("Ability Fun",function(value)
+    Say = value
 end)
 
-Main:Line()
+Fun:Toggle("Auto Spam",nil,function(value)
+Spam = value
+while wait(.1) do
+if Spam then
+local args = {
+    [1] = Say,
+    [2] = "All"
+}
 
-Main:Button("Destroy Left UpperLeg",function()
-game.Players.LocalPlayer.Character.LeftUpperLeg:Destroy()
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+end
+end
 end)
 
-Main:Button("Destroy Right UpperLeg",function()
-game.Players.LocalPlayer.Character.RightUpperLeg:Destroy()
-end)
-
-Main:Line()
-
-Main:Button("Destroy Right Foot",function()
-game.Players.LocalPlayer.Character.RightFoot:Destroy()
-end)
-
-Main:Button("Destroy Left Foot",function()
-game.Players.LocalPlayer.Character.LeftFoot:Destroy()
-end)
-
-Main:Line()
-
-Main:Button("Destroy Right Hand",function()
-game.Players.LocalPlayer.Character.RightHand:Destroy()
-end)
-
-Main:Button("Destroy Left Hand",function()
-game.Players.LocalPlayer.Character.LeftHand:Destroy()
-end)
-
-Main:Line()
-
-Main:Button("Destroy Left UpperArm",function()
-game.Players.LocalPlayer.Character.LeftUpperArm:Destroy()
-end)
-
-Main:Button("Destroy Right UpperArm",function()
-game.Players.LocalPlayer.Character.RightUpperArm:Destroy()
-end)
-
-Main:Line()
-
-local Lazy = GUI:Tap("Lazy")
-
-Lazy:Button("Reset Character",function()
-game.Players.LocalPlayer.Character.HumanoidRootPart:Destroy()
-end)
-
-local O = GUI:Tap("Others")
-
-O:Toggle("Ui By Synap 9Mill🖤🥀#9999",nil,function(value)
-Auto = value
-    while wait(.1) do
-        if Auto then
-            print('Ui By Synap 9Mill🖤🥀#9999')
+Server:Button("Server Hop",function()
+        local PlaceID = game.PlaceId
+    local AllIDs = {}
+    local foundAnything = ""
+    local actualHour = os.date("!*t").hour
+    local Deleted = false
+    function TPReturner()
+        local Site;
+        if foundAnything == "" then
+            Site = game.HttpService:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. PlaceID .. '/servers/Public?sortOrder=Asc&limit=100'))
+        else
+            Site = game.HttpService:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. PlaceID .. '/servers/Public?sortOrder=Asc&limit=100&cursor=' .. foundAnything))
+        end
+        local ID = ""
+        if Site.nextPageCursor and Site.nextPageCursor ~= "null" and Site.nextPageCursor ~= nil then
+            foundAnything = Site.nextPageCursor
+        end
+        local num = 0;
+        for i,v in pairs(Site.data) do
+            local Possible = true
+            ID = tostring(v.id)
+            if tonumber(v.maxPlayers) > tonumber(v.playing) then
+                for _,Existing in pairs(AllIDs) do
+                    if num ~= 0 then
+                        if ID == tostring(Existing) then
+                            Possible = false
+                        end
+                    else
+                        if tonumber(actualHour) ~= tonumber(Existing) then
+                            local delFile = pcall(function()
+                                -- delfile("NotSameServers.json")
+                                AllIDs = {}
+                                table.insert(AllIDs, actualHour)
+                            end)
+                        end
+                    end
+                    num = num + 1
+                end
+                if Possible == true then
+                    table.insert(AllIDs, ID)
+                    wait()
+                    pcall(function()
+                        -- writefile("NotSameServers.json", game:GetService('HttpService'):JSONEncode(AllIDs))
+                        wait()
+                        game:GetService("TeleportService"):TeleportToPlaceInstance(PlaceID, ID, game.Players.LocalPlayer)
+                    end)
+                    wait(.1)
+                end
+            end
         end
     end
+    function Teleport() 
+        while wait() do
+            pcall(function()
+                TPReturner()
+                if foundAnything ~= "" then
+                    TPReturner()
+                end
+            end)
+        end
+    end
+    Teleport()
+end)
+
+Server:Button("Rejoin",function()
+    game:GetService("TeleportService"):Teleport(game.PlaceId, game:GetService("Players").localPlayer)
 end)
