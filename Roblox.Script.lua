9878edd0e33eb6718dab6c394c5085d4c164b6c5
N0Fee1 <98322667+N0Fee1@users.noreply.github.com>
2022-03-02 22:17:02 +0700

    Update Roblox.Script.lua

--- Roblox.Script.lua
+++ Roblox.Script.lua
@@ -8,12 +8,13 @@ game:Shutdown()
 end
 
 wait(.1)
-local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Patskorn/GUI/main/Copy-SynapOver.lua"))()
-local GUI = library:new("","[ RightControl ]")
-local Main = GUI:Tap("Mian")
-local Cr = GUI:Tap("Credit")
+local DiscordLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/N0Fee1/Ui-TableHub/main/Ui.lua"))()
+local win = DiscordLib:Window("Blox Fruits")
+local Ui = win:Server("Ability Hub","")
+local Main = Ui:Channel("Main")
+local Cr = Ui:Channel("Credit")
 
-Main:TextBox("Message",function(value)
+Main:Textbox("Message", "Please, Enter Message Spam",value, function(value)
     Say = value
 end)
 
@@ -41,7 +42,7 @@ Instance.new("Sound", game:GetService("Workspace")).Name = "MUSIC_FUNCTION1"
 Instance.new("Sound", game:GetService("Workspace")).Volume = 5
 Instance.new("Sound", game:GetService("Workspace")).Looped = true
 
-Main:TextBox("Soun Id",function(value)
+Main:Textbox("Sound Id", "Pls Enter Soun Id", value, function(value)
     Music1 = value
 end)
 
@@ -60,10 +61,21 @@ Main:Button("Go ku",function()
     end)
 end)
 
-Main:TextBox("Heat Fire",function(value)
+Main:Textbox("Heat Fire", "Heat Fire( MAX1000 )", value, function(value)
     Heat = value
 end)
 
-Main:TextBox("Size Fire",function(value)
+Main:Textbox("Size Fire", "Size Fire( MAX1000 )", value, function(value)
     Size = value
 end)
+
+    Main:Textbox("Jump Power", "Speed Power( MAX1000 )", value, function(value)
+        game.Players.LocalPlayer.Character.Humanoid.JumpPower = value
+    end)
+
+   Main:Textbox("Speed Power", "Speed Power( MAX1000 )", value, function(value)
+        game:GetService("Players").LocalPlayer.Character.Humanoid.WalkSpeed  = value
+        while value do wait(1)
+            game:GetService("Players").LocalPlayer.Character.Movement.Disabled = true
+        end
+    end)
